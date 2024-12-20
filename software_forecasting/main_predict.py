3cfe7fb358249c16397fb34c0627d22c2b061379
apple <apple@appleui-MacBookAir.local>
2024-12-20 22:14:50 +0900

    update code

--- software_forecasting/main_predict.py
+++ software_forecasting/main_predict.py
@@ -4,6 +4,11 @@ from model import processing_data
 import tensorflow as tf
 from datetime import date
 
+# Set GPU memory growth (optional, only needed if you're using a GPU)
+#if tf.config.list_physical_devices('GPU'):
+#    physical_devices = tf.config.list_physical_devices('GPU')
+#    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 config = ConfigProto()
@@ -14,7 +19,7 @@ session = InteractiveSession(config=config)
 def perform_forecasting(data_in):
 
     temp = data_in
-    temp_model = tf.keras.models.load_model('temperature_stacked_lstm.h5')
+    temp_model = tf.keras.models.load_model('model/temperature_stacked_lstm.h5')
     temp_norm,scaler_temp = processing_data.proc_data(temp,feature_inputs = 'ta')
     temp_pred = temp_model.predict(temp_norm)
     temp_pred_rev = scaler_temp.inverse_transform(temp_pred).reshape(24,1)
